ef0a512043e6b014dd48db4dd9636dfe8ecbfb4b
Daniel Herzog <alu0100699494@ull.edu.es>
2014-02-19 15:26:57 +0000

    Se ha dado soporte para el localStorage. Tenemos que guardar el contenido del "input" y del "output", ya que javascript, por razones de seguridad, no permite modificar el contenido del input de ficheros. Así, al inicio de la carga, si hay algo guardado en el localStorage, se carga y se muestra en la página.

--- ini.js
+++ ini.js
@@ -1,6 +1,11 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
 $(document).ready(function() {
+	if (window.localStorage && localStorage.initialinput && localStorage.finaloutput) {
+	  out.className = 'unhidden';
+	  initialinput.innerHTML = localStorage.initialinput;
+	  finaloutput.innerHTML = localStorage.finaloutput;
+	}
    $("#fileinput").change(calculate);
 });
 
@@ -9,20 +14,25 @@ function calculate(evt) {
   var f = evt.target.files[0]; 
 
   if (f) {
-    var r = new FileReader();
-    r.onload = function(e) { 
-      var contents = e.target.result;
-      
-      var tokens = lexer(contents);
+	var r = new FileReader();
+	r.onload = function(e) { 
+	  var contents = e.target.result;
+	  
+	  var tokens = lexer(contents);
 	  var pretty = tokensToString(tokens);
-      
-      out.className = 'unhidden';
-      initialinput.innerHTML = contents;
-      finaloutput.innerHTML = pretty;
-    }
-    r.readAsText(f);
+	  
+	  out.className = 'unhidden';
+	  initialinput.innerHTML = contents;
+	  finaloutput.innerHTML = pretty;
+	  
+	  if (window.localStorage) {
+		localStorage.initialinput = contents;
+		localStorage.finaloutput = pretty;
+	  }
+	}
+	r.readAsText(f);
   } else { 
-    alert("Failed to load file");
+	alert("Failed to load file");
   }
 }
 
@@ -38,40 +48,40 @@ function tokensToString(tokens) {
 }
 
 function lexer(input) {
-  var blanks         = /^\s+/;
-  var iniheader      = /^\[([^\t\n]+)\](?=\s+)/;
-  var comments       = /^[;#](.*)/;
+  var blanks		 = /^\s+/;
+  var iniheader		 = /^\[([^\t\n]+)\](?=\s+)/;
+  var comments		 = /^[;#](.*)/;
   var nameEqualValue = /^([^=;\r\n]+)=([^;\r\n]*)/;
-  var any            = /^(.|\n)+/;
+  var any			 = /^(.|\n)+/;
 
   var out = [];
   var m = null;
 
   while (input != '') {
-    if (m = blanks.exec(input)) {
-      input = input.substr(m.index+m[0].length);
-      out.push({ type : 'blanks', match: m });
-    }
-    else if (m = iniheader.exec(input)) {
-      input = input.substr(m.index+m[0].length);
-      out.push({ type: 'header', match: m });
-    }
-    else if (m = comments.exec(input)) {
-      input = input.substr(m.index+m[0].length);
-      out.push({ type: 'comments', match: m });
-    }
-    else if (m = nameEqualValue.exec(input)) {
-      input = input.substr(m.index+m[0].length);
-      out.push({ type: 'nameEqualValue', match: m });
-    }
-    else if (m = any.exec(input)) {
-      out.push({ type: 'error', match: m });
-      input = '';
-    }
-    else {
-      alert("Fatal Error!"+substr(input,0,20));
-      input = '';
-    }
+	if (m = blanks.exec(input)) {
+	  input = input.substr(m.index+m[0].length);
+	  out.push({ type : 'blanks', match: m });
+	}
+	else if (m = iniheader.exec(input)) {
+	  input = input.substr(m.index+m[0].length);
+	  out.push({ type: 'header', match: m });
+	}
+	else if (m = comments.exec(input)) {
+	  input = input.substr(m.index+m[0].length);
+	  out.push({ type: 'comments', match: m });
+	}
+	else if (m = nameEqualValue.exec(input)) {
+	  input = input.substr(m.index+m[0].length);
+	  out.push({ type: 'nameEqualValue', match: m });
+	}
+	else if (m = any.exec(input)) {
+	  out.push({ type: 'error', match: m });
+	  input = '';
+	}
+	else {
+	  alert("Fatal Error!"+substr(input,0,20));
+	  input = '';
+	}
   }
   return out;
 }
