ad1a291883d2014c2e111854e4cd2e3dd9fb6963
Daniel Herzog <alu0100699494@ull.edu.es>
2014-02-20 00:25:57 +0000

    Se han arreglado las pruebas. Ahora funcionan correctamente.

--- tests/tests.js
+++ tests/tests.js
@@ -1,36 +1,41 @@
 var assert = chai.assert;
 
 suite('INI', function() {
-  
-    test('Primera prueba', function() {
-        var str_input = '; Comentario';
-	var tokens_in = [{match: ["; Comentario", "Comentario"], type: "comments"}];
+  test('Prueba de lexer.', function() {
+    var str_input = "; Comentario";
+	var tokens_in = JSON.stringify([ {type: "comments", match: ["; Comentario"," Comentario"] }]); 
 	var tokens_out = lexer(str_input);
-	assert.deepEqual(tokens_in, tokens_out);
-    });
+	
+	assert.deepEqual(tokens_in, JSON.stringify(tokens_out));
+  });
 
-    test('Entrada erronea', function() {
-        var str_input = '[ow\ner]';
+  test('Prueba de lexer: Entrada erronea', function() {
+    var str_input = "error";
+	var tokens_in = JSON.stringify([ {type: "error", match: ["error","r"] }]); 
 	var tokens_out = lexer(str_input);
-	assert.match(tokens_out, /'class=error'/);
-    });
+	
+	assert.deepEqual(tokens_in, JSON.stringify(tokens_out));
+  });
 
-    test('Probando tokensToString', function() {
-	var str_input = '[owner]';
+  test('Probando tokensToString', function() {
+	var str_input = "[owner]\r\n";
 	var tokens_out = lexer(str_input);
 	var prett = tokensToString(tokens_out);
-	assert.equal(prett, '<ol class="ol_output">  <li class = "comments"> <span> {  "type": "comments",  "match": [    "; Comentario",    " Comentario"  ]} </span></li>  </ol>');
-    });
+	assert.equal(prett, "<ol class=\"ol_output\">  <li class = \"header\"> <span> {\n  \"type\": \"header\",\n  \"match\": [\n    \"[owner]\",\n    \"owner\"\n  ]\n} </span></li>  <li class = \"blanks\"> <span> {\n  \"type\": \"blanks\",\n  \"match\": [\n    \"\\r\\n\"\n  ]\n} </span></li>  </ol>\n");
+  });
 
-    test('tokensToString con entrada erronea', function() {
-        var str_input = '[ow\ner]';
+  test('tokensToString con entrada erronea', function() {
+    var str_input = "error";
 	var tokens_out = lexer(str_input);
-        var prett = tokensToString(tokens_out);
-	assert.match(tokens_out, /'class=error'/);
-    });
+	var prett = tokensToString(tokens_out);
+	assert.equal(prett, "<ol class=\"ol_output\">  <li class = \"error\"> <span> {\n  \"type\": \"error\",\n  \"match\": [\n    \"error\",\n    \"r\"\n  ]\n} </span></li>  </ol>\n");
+  });
 
-    test('Probando localstorage', function() {
-	assert.match(window.localstorage, /'initialinput'/);
-    });
+  test('Probando localstorage', function() {
+    if(window.localStorage){
+	  localStorage.finaloutput = "test";
+	  assert.equal(localStorage.finaloutput, "test");
+	}
+  });
 
 });
